9ecf13e975c100b07424677ccee720f0f13f710b
ouattaraabraham <abraham.ouattara2019@gmail.com>
2025-05-15 14:54:10 +0100

    add section equipe

--- components/qui-sommes-nous/about-section2.tsx
+++ components/qui-sommes-nous/about-section2.tsx
@@ -1,13 +1,21 @@
+import Image from "next/image"
 
 export default function AboutSection2() {
   return (
-    <section className="pb-10 ">
+    <section className="pb-10 lg:pb-18 ppx-2 mmd:px-4 ">
       <div className="container  mx-auto px-4 md:px-4 lg:px-16">
-      <p className="text-zinc-600 my-6 leading-6 lg:leading-6">
+      <div className=" mb-6 font-bold">
+        <h1 className="font-bold ppx-2 ssm:px-4 llg:px-14 text-[24px] md:text-[30px] lg:text-[36px] my-6 lg:my-10 ">
+          L'immobilier en Afrique,
+          Autrement
+        </h1>
+      </div>
+
+      <p className="text-zinc-600 mb-6 leading-6 lg:leading-6">
             Luxury Home Abidjan est l'histoire d'un ensemble de collaborateurs qui ont une passion commune pour la
             pierre. Notre but est d'exprimer notre propre vision de l'immobilier en Afrique. Nous sommes une agence
             animée par une vingtaine de collaborateurs et nous partageons tous un point commun : la volonté de mettre le
-            client au cœur de notre métier.<br/><br className="lg:hidden"/>À votre service, une équipe diversifiée internationale qui applique la
+            client au cœur de notre métier.<br/><br/>À votre service, une équipe diversifiée internationale qui applique la
             philosophie LHA à tous les domaines de l'immobilier : que ce soit à l'achat, à la vente, à la location, la
             gestion, la commercialisation, le syndic et le conseil. Notre approche est basée sur l'écoute, en effet
             notre conviction première est l'intérêt du client.<br/><br className="lg:hidden"/>  Chez Luxury Home Abidjan, une transaction immobilière
--- components/qui-sommes-nous/equipe.tsx
+++ components/qui-sommes-nous/equipe.tsx
@@ -2,6 +2,7 @@
 import { Instagram, Linkedin, Twitter } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { string } from "zod";
 
 const data = [
 {
