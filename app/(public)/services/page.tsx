49ec9beaca9d9d23b5233315cb4d5c3988a3e8d3
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2025-10-22 17:51:22 +0000

    Ajustement des importations

--- app/(public)/services/page.tsx
+++ app/(public)/services/page.tsx
@@ -1,10 +1,6 @@
-import LogoShowcase from "@/components/home/logo-showcase/logo-showcase";
-import TestimonialsSection from "@/components/qui-sommes-nous/testimonials/testimonials-section";
-import HeroSection from "@/components/page-service/hero/hero-section";
 import ServicesBody from "@/components/page-service/service-body";
 import BlogSection from "@/components/qui-sommes-nous/blog/blog-section";
 import HeroService from "@/components/page-service/hero-sevices";
-// import ServiceBody from "@/components/service/servicebody/servicebody2";
 
 
 export default function Service(){
