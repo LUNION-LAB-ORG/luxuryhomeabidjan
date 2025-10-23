217c103144d52eaed8ea36b61cb314c4acf5aefb
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2025-10-23 13:39:29 +0000

    Add ServicesCarousel component and integrate it into services section

---
+++ components/home/services-section/services-carousel.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {motion, AnimatePresence} from 'motion/react';
+
+function ServicesCarousel({images}: { images: string[] }) {
+	return (
+		<div className="overflow-hidden relative">
+
+		</div>
+	);
+}
+
+export default ServicesCarousel;
